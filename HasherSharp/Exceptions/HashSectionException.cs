b2dd069c0b71e9ac16135be04e706697fbf9f1be
Maxim Verreault <verreault.maxim@gmail.com>
2017-01-10 21:27:16 -0500

    Configuration & Exceptions
    1 - Configuration class. Allows to create custom configuration sections
    within the AppConfig or WebConfig
    
    2 - Custom Hash Exception

---
+++ HasherSharp/Exceptions/HashSectionException.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HasherSharp.Exceptions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class HashSectionException : HashException
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public HashSectionException()
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
+        public HashSectionException(string message) : base(message)
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="innerException"></param>
+        public HashSectionException(string message, Exception innerException)  : base(message, innerException)
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="section"></param>
+        public HashSectionException(string message, string section) : base(message, section)
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="section"></param>
+        /// <param name="innerException"></param>
+        public HashSectionException(string message, string section, Exception innerException) : base(message, section, innerException)
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="context"></param>
+        protected HashSectionException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
